a8c73e537702eab514f26d1d3e3b45f750f36832
anafersan <105824182+anafersan@users.noreply.github.com>
2022-05-19 00:15:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,26 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 
 st.area_chart(chart_data)
+
+# Gráfico tarta
+
+import matplotlib.pyplot as plt
+
+#arr = np.random.normal(1, 1, size=100)
+#fig, ax = plt.subplots()
+#ax.hist(arr, bins=20)
+
+labels = 'Positive', 'Neutral', 'Negative'
+sections = [50, 35, 15]
+colors = ['g', 'y', 'r']
+
+fig = plt.pie(sections, labels=labels, colors=colors,
+        startangle=90,
+        explode = (0, 0, 0),
+        autopct = '%1.2f%%')
+
+#plt.axis('equal') # Try commenting this out.
+#plt.title('Pie Chart Twitter Sentiment Example')
+#plt.show()
+
+st.pyplot(fig)
