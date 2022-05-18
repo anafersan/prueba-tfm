769cc0d6a1fb091f68c56aa891b9628be2b04149
anafersan <105824182+anafersan@users.noreply.github.com>
2022-05-19 00:10:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 
 
 import streamlit as st
-st.title('Twitter')
+st.title('Twitter prueba')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)
