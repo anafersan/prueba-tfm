f5d13156797a50e08725a723594dba8cb7fd2988
anafersan <105824182+anafersan@users.noreply.github.com>
2022-05-19 10:07:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,22 @@ st.write("PRUEBA PARA TFM TWITTER")
 hashtag = st.text_input('Introduce un hashtag', '#Love')
 
 # Model importing
-tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+# Load the model (only executed once!)
+# NOTE: Don't set ttl or max_entries in this case
+
+@st.cache
+def load_model():
+	  return AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+  
+@st.cache
+def load_tokenizer():
+  return AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+  
+model = load_model()
+tokenizer = load_tokenizer()
+
+#tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+#model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
 sentiment_task = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
 # Twitter API client
