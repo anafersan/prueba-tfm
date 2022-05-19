ed752679712d1a943eb534c28036e0c06538200f
anafersan <105824182+anafersan@users.noreply.github.com>
2022-05-19 10:08:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,11 @@ hashtag = st.text_input('Introduce un hashtag', '#Love')
 
 @st.cache
 def load_model():
-	  return AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+	return AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
   
 @st.cache
 def load_tokenizer():
-  return AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+	return AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
   
 model = load_model()
 tokenizer = load_tokenizer()
