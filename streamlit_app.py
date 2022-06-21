cf3014fa12060f2008bb58fd7551f9282dabb6ad
anafersan <105824182+anafersan@users.noreply.github.com>
2022-06-21 23:10:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,11 @@ with st.container():
 		df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]})
 		options_builder = GridOptionsBuilder.from_dataframe(df)
 		options_builder.configure_column("col1", editable=True) 
-		options_builder.configure_selection("single") grid_options = options_builder.build()
+		options_builder.configure_selection("single")
+		grid_options = options_builder.build()
 
-		grid_return = AgGrid(df, grid_options) selected_rows = grid_return[“selected_rows”]
+		grid_return = AgGrid(df, grid_options) 
+		selected_rows = grid_return[“selected_rows”]
 
 		st.write(selected_rows)
 
