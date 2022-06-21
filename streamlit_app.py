d35b213f732d2a081a357710b22c2db58e4b5a62
anafersan <105824182+anafersan@users.noreply.github.com>
2022-06-21 23:06:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,9 @@ with st.container():
 		}
 		grid_return = AgGrid(df, grid_options)
 		new_df = grid_return["data"]
-		st.write(new_df)
+		selected_rows = grid_return[“selected_rows”]
+		st.write(selected_rows)
+
 
 	with col2:
 		# DESCRIPCIÓN
