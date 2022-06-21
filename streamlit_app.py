fee74120d2ec12c959d200c949318323fea287aa
anafersan <105824182+anafersan@users.noreply.github.com>
2022-06-21 22:12:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,51 +8,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import pipeline
-from streamlit_ace import st_ace
+from st_aggrid import AgGrid
 #import altair as alt
 
 
 with st.container():
+	
+	# TITULO
+	st.title('Twitter prueba')
 
-    # TITULO
-    st.title('Twitter prueba')
+	col1, col2 = st.columns([2, 3])
 
-    col1, col2 = st.columns([2, 3])
+	with col1:
+		# HEADER COL 1
+		st.subheader("LISTADO DE TT")
 
-    with col1:
-        # HEADER COL 1
-        st.subheader("LISTADO DE TT")
-        content = st_ace()
-        content
-
-    with col2:
-        # DESCRIPCIÓN
-        #st.write("PRUEBA PARA TFM TWITTER")
-        # FORMULARIO HASHTAG
-        hashtag = st.text_input('Introduce un hashtag', '#Love')
+	with col2:
+		# DESCRIPCIÓN
+		#st.write("PRUEBA PARA TFM TWITTER")
+		# FORMULARIO HASHTAG
+		hashtag = st.text_input('Introduce un hashtag', '#Love')
 
 # Model importing
 # Load the model (only executed once!)
 # NOTE: Don't set ttl or max_entries in this case
 
-
 @st.cache
 def load_model():
-    return AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-
-# @st.cache
-# def load_tokenizer():
+	return AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+  
+#@st.cache
+#def load_tokenizer():
 #	return AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-
-
+  
 model = load_model()
 #tokenizer = load_tokenizer()
 
-tokenizer = AutoTokenizer.from_pretrained(
-    "cardiffnlp/twitter-roberta-base-sentiment-latest")
+tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
 #model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-sentiment_task = pipeline("sentiment-analysis",
-                          model=model, tokenizer=tokenizer)
+sentiment_task = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
 # Twitter API client
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAGcdZgEAAAAADAbPiGFS3jT3w7bTECCewFv0oR8%3D4ZAl4MWV5Z8RmSrM99HaA5xhij9UkDxlYugx4tv8YO9Z8PcU0v"
@@ -61,34 +55,32 @@ consumer_secret = "n9hqzC2j8KPuqQKwa6hJkM3eBPvN1fwE2Gahr1riygX1LkahUq"
 access_token = "1496230187687624709-t1AcjFLOJfRk0zbvKkqYtqRv7uuLiD"
 access_token_secret = "NwnmURf8nmm4VnoDVLZwPqxPwXIRmamf5RgtYY4aXmmjn"
 
-client = tweepy.Client(bearer_token,
-                       consumer_key,
-                       consumer_secret,
-                       access_token,
+client = tweepy.Client(bearer_token, 
+                       consumer_key, 
+                       consumer_secret, 
+                       access_token, 
                        access_token_secret)
 
-# Tweets extraction
-query = hashtag + " lang:en -is:retweet"
+#Tweets extraction
+query =  hashtag + " lang:en -is:retweet" 
 
 response = client.search_recent_tweets(query=query,
-                                       tweet_fields=[
-                                           "created_at", "text", "source", "public_metrics", "entities"],
-                                       user_fields=[
-                                           "name", "username", "location", "verified", "description", "public_metrics"],
-                                       max_results=100,
-                                       expansions='author_id'
-                                       )
+                                     tweet_fields = ["created_at", "text", "source", "public_metrics", "entities"],
+                                     user_fields = ["name", "username", "location", "verified", "description", "public_metrics"],
+                                     max_results = 100,
+                                     expansions='author_id'
+                                     )
 
-# Creamos lista vacía
+#Creamos lista vacía
 sentimientos = []
 
-# Obtenemos sentimientos de los tweets y almacenamos en la lista
+#Obtenemos sentimientos de los tweets y almacenamos en la lista
 for tweet in response.data:
-    texto = tweet.text
-    sentiment = sentiment_task(texto)[0]
-    label = sentiment['label']
-    # print(label)
-    sentimientos[len(sentimientos):] = [label]
+  texto = tweet.text
+  sentiment = sentiment_task(texto)[0]
+  label = sentiment['label']
+  #print(label)
+  sentimientos[len(sentimientos):] = [label]
 
 
 # Gráfico distribución de sentimientos
@@ -97,18 +89,19 @@ for tweet in response.data:
 #sections = [sentimientos.count('Positive'), sentimientos.count('Neutral'), sentimientos.count('Negative')]
 #colors = ['g', 'y', 'r']
 #chart_data = pd.DataFrame([sections], columns = labels)
-# st.bar_chart(chart_data)
+#st.bar_chart(chart_data)
 
 with col2:
-    labels = 'Positive', 'Neutral', 'Negative'
-    sections = [sentimientos.count('Positive'), sentimientos.count(
-        'Neutral'), sentimientos.count('Negative')]
-    colors = ['g', 'y', 'r']
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sections, labels=labels, colors=colors,
-            startangle=90,
-            explode=(0, 0, 0),
-            autopct='%1.2f%%')
-    ax1.axis('equal')  # Try commenting this out.
-    st.write("Pie Chart Twitter Sentiment Example")
-    st.pyplot(fig1)
+	labels = 'Positive', 'Neutral', 'Negative'
+	sections = [sentimientos.count('Positive'), sentimientos.count('Neutral'), sentimientos.count('Negative')]
+	colors = ['g', 'y', 'r']
+	fig1, ax1 = plt.subplots()
+	ax1.pie(sections, labels=labels, colors=colors,
+		startangle=90,
+		explode = (0, 0, 0),
+		autopct = '%1.2f%%')
+	ax1.axis('equal') # Try commenting this out.
+	st.write("Pie Chart Twitter Sentiment Example")
+	st.pyplot(fig1)
+
+
