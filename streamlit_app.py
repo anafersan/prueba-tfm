c1668e12efa6f9c0f690944d410b0126b0da4b2b
anafersan <105824182+anafersan@users.noreply.github.com>
2022-06-22 18:28:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import pandas as pd
 #import plotly.figure_factory as ff
@@ -6,31 +7,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import pipeline
-from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid import AgGrid, GridOptionsBuilder, DataReturnMode
 #import altair as alt
-
-
+return_mode = st.sidebar.selectbox("Return Mode", list(DataReturnMode.__members__), index=1)
+return_mode_value = DataReturnMode.__members__[return_mode]
 with st.container():
 	
 	# TITULO
 	st.title('Twitter prueba')
-
 	col1, col2 = st.columns([2, 3])
 	selected_rows = []
-
 	with col1:
 		# HEADER COL 1
 		st.subheader("LISTADO DE TT")
 		df = pd.DataFrame({"hashtag": ["#love", "#ukranie", "#madrid", "#dog", "#anathebest"]})
 		options_builder = GridOptionsBuilder.from_dataframe(df)
 		options_builder.configure_column('hashtag', value=True, editable=False) 
+		options_builder.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True)
+		options_builder.configure_column("hashtag", editable=False) 
+		options_builder.configure_pagination(paginationAutoPageSize=True)
 		options_builder.configure_selection("single", use_checkbox=True)
 		use_checkbox = st.sidebar.checkbox("Use check box for selection", value=True)
+
 		grid_options = options_builder.build()
 
-		grid_return = AgGrid(df, grid_options) 
+		grid_return = AgGrid(
+			df, 
+			grid_options,
+			width='100%',
+			data_return_mode=return_mode_value
+
+			width='100%',			
+		) 
+
+		df = grid_return['data']
 		selected_rows = grid_return["selected_rows"]
 		st.write(selected_rows)
+		selected_df = pd.DataFrame(selected).apply(pd.to_numeric, errors='coerce')
+		st.write(selected_df)
 
 
 	with col2:
@@ -42,11 +56,9 @@ with st.container():
 		else:
 			#hashtag = st.text_input('Introduce un hashtag', getattr(selected_rows[0],'hashtag'))
 			hashtag = st.text_input('Introduce un hashtag', selected_rows[0]["hashtag"])
-
 # Model importing
 # Load the model (only executed once!)
 # NOTE: Don't set ttl or max_entries in this case
-
 @st.cache
 def load_model():
 	return AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
@@ -57,37 +69,30 @@ def load_model():
   
 model = load_model()
 #tokenizer = load_tokenizer()
-
 tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
 #model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
 sentiment_task = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-
 # Twitter API client
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAGcdZgEAAAAADAbPiGFS3jT3w7bTECCewFv0oR8%3D4ZAl4MWV5Z8RmSrM99HaA5xhij9UkDxlYugx4tv8YO9Z8PcU0v"
 consumer_key = "3UkoMlYsWmPeUDYyTpDHb0V62"
 consumer_secret = "n9hqzC2j8KPuqQKwa6hJkM3eBPvN1fwE2Gahr1riygX1LkahUq"
 access_token = "1496230187687624709-t1AcjFLOJfRk0zbvKkqYtqRv7uuLiD"
 access_token_secret = "NwnmURf8nmm4VnoDVLZwPqxPwXIRmamf5RgtYY4aXmmjn"
-
 client = tweepy.Client(bearer_token, 
                        consumer_key, 
                        consumer_secret, 
                        access_token, 
                        access_token_secret)
-
 #Tweets extraction
 query =  hashtag + " lang:en -is:retweet" 
-
 response = client.search_recent_tweets(query=query,
                                      tweet_fields = ["created_at", "text", "source", "public_metrics", "entities"],
                                      user_fields = ["name", "username", "location", "verified", "description", "public_metrics"],
                                      max_results = 100,
                                      expansions='author_id'
                                      )
-
 #Creamos lista vacía
 sentimientos = []
-
 #Obtenemos sentimientos de los tweets y almacenamos en la lista
 for tweet in response.data:
   texto = tweet.text
@@ -95,16 +100,12 @@ for tweet in response.data:
   label = sentiment['label']
   #print(label)
   sentimientos[len(sentimientos):] = [label]
-
-
 # Gráfico distribución de sentimientos
-
 #labels = ['Positive', 'Neutral', 'Negative']
 #sections = [sentimientos.count('Positive'), sentimientos.count('Neutral'), sentimientos.count('Negative')]
 #colors = ['g', 'y', 'r']
 #chart_data = pd.DataFrame([sections], columns = labels)
 #st.bar_chart(chart_data)
-
 with col2:
 	labels = 'Positive', 'Neutral', 'Negative'
 	sections = [sentimientos.count('Positive'), sentimientos.count('Neutral'), sentimientos.count('Negative')]
@@ -117,4 +118,3 @@ with col2:
 	ax1.axis('equal') # Try commenting this out.
 	st.write("Pie Chart Twitter Sentiment Example")
 	st.pyplot(fig1)
-
