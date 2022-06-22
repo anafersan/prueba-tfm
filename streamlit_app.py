37452ecf14b167affed43b9cfaec1d879d054bf8
anafersan <105824182+anafersan@users.noreply.github.com>
2022-06-22 22:39:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,109 +1,75 @@
-
-
 import streamlit as st
-import pandas as pd
-#import plotly.figure_factory as ff
-import tweepy
-import matplotlib.pyplot as plt
 import numpy as np
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from transformers import pipeline
-from st_aggrid import AgGrid, GridOptionsBuilder
-#import altair as alt
+import pandas as pd
 
-with st.container():
-	
-	# TITULO
-	st.title('Twitter prueba')
-	col1, col2 = st.columns([2, 3])
-	selected_rows = []
-	with col1:
-		# HEADER COL 1
-		st.subheader("LISTADO DE TT")
-		dataHastag = {
-			"col1": ["love", "ukranie", "madrid", "dog", "anathebest"],
-			"col2": ["love", "ukranie", "madrid", "dog", "anathebest"]
-		}
-		df = pd.DataFrame(dataHastag)
-		options_builder = GridOptionsBuilder.from_dataframe(df)
-		options_builder.configure_default_column(groupable=True, value=True, enableRowGroup=True, editable=True)
-		options_builder.configure_column("col1", type=["stringColumn","stringColumnFilter"])
-		options_builder.configure_selection("single", use_checkbox=True)
-		options_builder.configure_pagination(paginationAutoPageSize=True)
-		options_builder.configure_grid_options(domLayout='normal')
-		grid_options = options_builder.build()
-		grid_return = AgGrid(df, grid_options)
-		st.write(grid_options)
-		st.write(grid_return)
-		
-		
-	with col2:
-		# DESCRIPCIÓN
-		st.write("PRUEBA PARA TFM TWITTER")
-		# FORMULARIO HASHTAG
-		if len(selected_rows) == 0:
-			hashtag = st.text_input('Introduce un hashtag', "#love")
-		else:
-			hashtag = st.text_input('Introduce un hashtag', selected_rows[0]["hashtag"])
-# Model importing
-# Load the model (only executed once!)
-# NOTE: Don't set ttl or max_entries in this case
-@st.cache
-def load_model():
-	return AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-  
-#@st.cache
-#def load_tokenizer():
-#	return AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-  
-model = load_model()
-#tokenizer = load_tokenizer()
-tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-#model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
-sentiment_task = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-# Twitter API client
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAGcdZgEAAAAADAbPiGFS3jT3w7bTECCewFv0oR8%3D4ZAl4MWV5Z8RmSrM99HaA5xhij9UkDxlYugx4tv8YO9Z8PcU0v"
-consumer_key = "3UkoMlYsWmPeUDYyTpDHb0V62"
-consumer_secret = "n9hqzC2j8KPuqQKwa6hJkM3eBPvN1fwE2Gahr1riygX1LkahUq"
-access_token = "1496230187687624709-t1AcjFLOJfRk0zbvKkqYtqRv7uuLiD"
-access_token_secret = "NwnmURf8nmm4VnoDVLZwPqxPwXIRmamf5RgtYY4aXmmjn"
-client = tweepy.Client(bearer_token, 
-                       consumer_key, 
-                       consumer_secret, 
-                       access_token, 
-                       access_token_secret)
-#Tweets extraction
-query =  hashtag + " lang:en -is:retweet" 
-response = client.search_recent_tweets(query=query,
-                                     tweet_fields = ["created_at", "text", "source", "public_metrics", "entities"],
-                                     user_fields = ["name", "username", "location", "verified", "description", "public_metrics"],
-                                     max_results = 100,
-                                     expansions='author_id'
-                                     )
-#Creamos lista vacía
-sentimientos = []
-#Obtenemos sentimientos de los tweets y almacenamos en la lista
-for tweet in response.data:
-  texto = tweet.text
-  sentiment = sentiment_task(texto)[0]
-  label = sentiment['label']
-  #print(label)
-  sentimientos[len(sentimientos):] = [label]
-# Gráfico distribución de sentimientos
-#labels = ['Positive', 'Neutral', 'Negative']
-#sections = [sentimientos.count('Positive'), sentimientos.count('Neutral'), sentimientos.count('Negative')]
-#colors = ['g', 'y', 'r']
-#chart_data = pd.DataFrame([sections], columns = labels)
-#st.bar_chart(chart_data)
-with col2:
-	labels = 'Positive', 'Neutral', 'Negative'
-	sections = [sentimientos.count('Positive'), sentimientos.count('Neutral'), sentimientos.count('Negative')]
-	colors = ['g', 'y', 'r']
-	fig1, ax1 = plt.subplots()
-	ax1.pie(sections, labels=labels, colors=colors,
-		startangle=90,
-		explode = (0, 0, 0),
-		autopct = '%1.2f%%')
-	ax1.axis('equal') # Try commenting this out.
-	st.write("Pie Chart Twitter Sentiment Example")
-	st.pyplot(fig1)
+from st_aggrid import AgGrid, DataReturnMode, GridUpdateMode, GridOptionsBuilder
+
+@st.cache()
+def get_data_ex6(rows=None):
+    if not rows:
+        rows = np.random.randint(1,10)
+    df = pd.DataFrame(
+        np.random.randint(0, 100, 5*rows).reshape(-1, 5), columns= list("abcde")
+    )
+    return df
+
+st.sidebar.markdown("example controls:")
+use_fixed_key = st.sidebar.checkbox("Use fixed key in AgGrid call", value=True)
+
+button = st.sidebar.button("Generate random data")
+use_10_rows = st.sidebar.checkbox("Fix data to 10 rows.", value=True)
+if use_10_rows:
+    rows = 10
+else:
+    rows = None
+
+
+height = st.sidebar.slider('Height', min_value=100, max_value=800, value=400)
+reload_data = False
+
+if button:
+    from streamlit import caching
+    caching.clear_cache()
+    reload_data=True
+
+data = get_data_ex6(rows)
+gb = GridOptionsBuilder.from_dataframe(data)
+#make all columns editable
+gb.configure_columns(list('abcde'), editable=True)
+
+#Create a calculated column that updates when data is edited. Use agAnimateShowChangeCellRenderer to show changes   
+#gb.configure_column('row total', valueGetter='Number(data.a) + Number(data.b) + Number(data.c) + Number(data.d) + Number(data.e)', cellRenderer='agAnimateShowChangeCellRenderer', editable='false', type=['numericColumn'])
+go = gb.build()
+
+st.subheader("Fixing key parameter")
+st.markdown("""
+Setting a fixed key for the component will prevent the grid to reinitialize when dataframe parameter change, simulated here 
+by pressing the button on the side bar.  
+Data will only be refreshed when the parameter reload_data is set to True
+""")
+
+
+if use_fixed_key:
+    st.markdown(f"Grid was called as: <br>```AgGrid(..., reload_data={reload_data})``` <br>", unsafe_allow_html=True)
+    ag = AgGrid(
+        data, 
+        gridOptions=go, 
+        height=height, 
+        fit_columns_on_grid_load=True, 
+        key='an_unique_key',
+        reload_data=reload_data
+    )
+else:
+    st.markdown(f"Grid was called as: <br>```AgGrid(...)``` <br>", unsafe_allow_html=True)
+    ag = AgGrid(
+        data, 
+        gridOptions=go, 
+        height=height, 
+        fit_columns_on_grid_load=True
+    )
+
+st.subheader("Returned Data")
+st.dataframe(ag['data'])
+
+st.subheader("Grid Options")
+st.write(go)```
