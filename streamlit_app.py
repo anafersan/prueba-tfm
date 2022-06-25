fac335bea8fd5676df1e9870d7d0fbeebbe6813c
anafersan <105824182+anafersan@users.noreply.github.com>
2022-06-25 19:08:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import pipeline
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 import yweather
-import json
 
 #import altair as alt
 
