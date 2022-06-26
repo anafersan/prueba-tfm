003d05464fd2b9ca5a087ec92cc0b2371e848d66
anafersan <105824182+anafersan@users.noreply.github.com>
2022-06-26 23:15:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,8 @@ with st.container():
 		#if len(selected_rows) == 0:
 		#	hashtag = st.text_input('Introduce un hashtag', "#love")
 		#else:
-		hashtag = st.text_input('Introduce un hashtag', selected_rows[0]["hashtag"])
+		#hashtag = st.text_input('Introduce un hashtag', selected_rows[0]["hashtag"])
+		hashtag = selected_rows[0]["hashtag"]
 				
 
 # Model importing
