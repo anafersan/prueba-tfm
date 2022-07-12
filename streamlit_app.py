e140b4ba470b13bd0951f4ea10faab0376fa0728
anafersan <105824182+anafersan@users.noreply.github.com>
2022-07-13 00:58:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,16 +51,19 @@ def get_weoid():
 df_woeid = get_weoid()
 
 # Variable que indica cambio
-CHANGE = 0
+#CHANGE = 0
 
+
+#@st.cache(allow_output_mutation=True)
+#def set_save_localization(ubicacion):
+#	saved_localizacion = ubicacion
+#	return saved_localizacion
 
 @st.cache(allow_output_mutation=True)
-def set_save_localization(ubicacion):
-	saved_localizacion = ubicacion
-	return saved_localizacion
+saved_localizacion = ""
 
-saved_localizacion = set_save_localization("")
-
+@st.cache(allow_output_mutation=True)
+saved_trending = ""
 
 
 with st.container():
@@ -79,15 +82,11 @@ with st.container():
 		lista_lugares.sort()
 		wordwide_position = lista_lugares.index("Worldwide")
 		option_localizacion = st.selectbox("Ubicaciones disponibles:", lista_lugares, index=wordwide_position)
-		if option_localizacion != saved_localizacion:
-			CHANGE = 1
-			saved_localizacion = set_save_localization(option_localizacion)
-			
-			
-		
-if CHANGE == 1:
-	#Se vuelve a poner la variable a 0
-	CHANGE = 0
+
+							
+if option_localizacion != saved_localizacion:
+
+	saved_localizacion = option_localizacion
 	
 	# Twitter API client - v1 - BUSQUEDA DE TENDENCIAS
 	consumer_keyV1 = "ehHCNZacFhnw6IX0GNhtcheXy"
@@ -119,34 +118,34 @@ if CHANGE == 1:
 	hashtags_df = pd.DataFrame(hashtags_info_ls)
 
 
-	# Se listan los hashtags
-	with col1:
-		# HEADER COL 1 - PART 2
-		st.header("2. Selecciona una tendencia")
+# Se listan los hashtags
+with col1:
+	# HEADER COL 1 - PART 2
+	st.header("2. Selecciona una tendencia")
 
-		#df = pd.DataFrame([{"hashtag": ["#love"]}, {"hashtag": ["#love"]}, {"hashtag": ["#love"]}])
-		opciones_tendencias = hashtags_df['hs_name']
-		df_tendencias = hashtags_df[['hs_name']]
-		#tendencia_select = st.checkbox(opciones_tendencias, True)
-		#print("HOLI SOY EL BOTON DE MIERDA")
-		tendencia_select = st.radio("Seleccciona una opción", opciones_tendencias)
+	#df = pd.DataFrame([{"hashtag": ["#love"]}, {"hashtag": ["#love"]}, {"hashtag": ["#love"]}])
+	opciones_tendencias = hashtags_df['hs_name']
+	df_tendencias = hashtags_df[['hs_name']]
+	#tendencia_select = st.checkbox(opciones_tendencias, True)
+	#print("HOLI SOY EL BOTON DE MIERDA")
+	tendencia_select = st.radio("Seleccciona una opción", opciones_tendencias)
 
-		#options_builder = GridOptionsBuilder.from_dataframe(df_tendencias)
-		#options_builder.configure_default_column(groupable=True, value=True, enableRowGroup=True, editable=True)
-		#options_builder.configure_column("hs_name", type=["stringColumn","stringColumnFilter"])
-		#options_builder.configure_selection("single", use_checkbox=True)
-		#options_builder.configure_pagination(paginationAutoPageSize=True)
-		#options_builder.configure_grid_options(domLayout='normal')
-		#grid_options = options_builder.build()
-		#grid_return = AgGrid(df, grid_options, update_mode="MODEL_CHANGED")
-		#selected_rows = grid_return["selected_rows"]
+	#options_builder = GridOptionsBuilder.from_dataframe(df_tendencias)
+	#options_builder.configure_default_column(groupable=True, value=True, enableRowGroup=True, editable=True)
+	#options_builder.configure_column("hs_name", type=["stringColumn","stringColumnFilter"])
+	#options_builder.configure_selection("single", use_checkbox=True)
+	#options_builder.configure_pagination(paginationAutoPageSize=True)
+	#options_builder.configure_grid_options(domLayout='normal')
+	#grid_options = options_builder.build()
+	#grid_return = AgGrid(df, grid_options, update_mode="MODEL_CHANGED")
+	#selected_rows = grid_return["selected_rows"]
 
-		# FORMULARIO HASHTAG
-		#if len(selected_rows) == 0:
-		#	hashtag = st.text_input('Introduce un hashtag', "#love")
-		#else:
-		#hashtag = st.text_input('Introduce un hashtag', selected_rows[0]["hashtag"])
-		#hashtag = selected_rows[0]["hashtag"]
+	# FORMULARIO HASHTAG
+	#if len(selected_rows) == 0:
+	#	hashtag = st.text_input('Introduce un hashtag', "#love")
+	#else:
+	#hashtag = st.text_input('Introduce un hashtag', selected_rows[0]["hashtag"])
+	#hashtag = selected_rows[0]["hashtag"]
 				
 
 # Model importing
