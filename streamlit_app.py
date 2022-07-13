2b216f1ed3626e09612cb21b249543e20ded5215
anafersan <105824182+anafersan@users.noreply.github.com>
2022-07-13 21:14:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import pipeline
-from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+#from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 #import yweather
 import json
 from datetime import datetime
